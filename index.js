0102107875e857dc84358dc70148f6e151ba4dd2
romain.piveteau <r.piveteau@outlook.com>
2023-06-08 16:07:12 +0200

    toto exo 3 part 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /** Part for the starting instanciation */
 /** creating the hero Heracles */
-const heracles = new Fighter('👨 Heracles', 20, 6, './images/heracles.svg');
+const heracles = new Hero('👨 Heracles', 20, 6, './images/heracles.svg', {x: 5, y: 5});
 
 /** Creating his weapon and associating it */
 const weapon = new Weapon('sword', 10, './images/sword.svg');
@@ -11,9 +11,13 @@ const shield = new Shield('shield', 10, './images/shield.svg');
 heracles.shield = shield;
 
 /** Creating all of his adversaries */
-const bird1 = new Fighter('Bird', 25, 12, './images/bird.svg');
-const bird2 = new Fighter('Bird', 25, 12, './images/bird.svg');
-const bird3 = new Fighter('Bird', 25, 12, './images/bird.svg');
+const bird1 = new Monster('Bird', 25, 12, './images/bird.svg', {x: 8, y: 2});
+const bird3 = new Monster('Bird', 25, 12, './images/bird.svg', {x: 4, y: 9});
+const bird2 = new Monster('Bird', 25, 12, './images/bird.svg', {x: 5, y: 1});
+
+const arena = new Arena(heracles, [bird1, bird2, bird3]);
+const ArenaHTML = new ArenaTemplate('arena');
+ArenaHTML.createArena(arena);
 
 /** Creating the hero section in the html */
 const fighterHtml = new FightersTemplate('fighters');
---
+++ src/Arena.js
@@ -0,0 +1,13 @@
+
+class Arena {
+    monsters;
+    hero;
+    size;
+
+    constructor(hero, monsters) {
+        this.hero = hero;
+        this.monsters = monsters;
+        this.size = 10;
+    }
+
+}
